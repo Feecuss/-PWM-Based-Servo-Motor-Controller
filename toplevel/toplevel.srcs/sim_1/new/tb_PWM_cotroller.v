88abbcf278f2abe7233b11148fbd50d5b20c8dd7
Feecuss <165298254+Feecuss@users.noreply.github.com>
2024-04-23 11:03:20 +0200

    top_level

---
+++ toplevel/toplevel.srcs/sim_1/new/tb_PWM_cotroller.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/23/2024 11:00:44 AM
+// Design Name: 
+// Module Name: tb_PWM_cotroller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_PWM_cotroller(
+
+    );
+endmodule
