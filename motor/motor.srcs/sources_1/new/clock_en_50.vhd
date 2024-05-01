9822dedf8c9e32912bfd1c0570282003c225bf00
Feecuss <165298254+Feecuss@users.noreply.github.com>
2024-05-01 14:07:24 +0200

    Update clock_en_50.vhd

--- motor/motor.srcs/sources_1/new/clock_en_50.vhd
+++ motor/motor.srcs/sources_1/new/clock_en_50.vhd
@@ -8,49 +8,36 @@ use IEEE.std_logic_unsigned.all;
 entity clk_en_50 is
 
     generic(
-            PERIOD : integer := 2000000 -- 20ms/50Hz
+            PERIOD : integer := 2000000 --determines the length of the periodic signal [20ms/50Hz]
             );
 
-    Port ( clk      : in STD_LOGIC;
-           rst      : in STD_LOGIC;
-           pulse    : out STD_LOGIC);
+    Port ( clk      : in STD_LOGIC; --internal clock of 100MHz
+           rst      : in STD_LOGIC; --reset
+           pulse    : out STD_LOGIC); --output pulse
 end clk_en_50;
 
 ----------------------------------------------------------------------------------
 
 architecture Behavioral of clk_en_50 is
 
-    constant BITS_NEEDED : integer := integer(ceil(log2(real(PERIOD+1))));
-    signal sig_count : std_logic_vector(BITS_NEEDED - 1 downto 0);
+    constant BITS_NEEDED : integer := integer(ceil(log2(real(PERIOD+1)))); --calculating the number of bits needed to represent a given period
+    signal sig_count : std_logic_vector(BITS_NEEDED - 1 downto 0); --count pulses that have a length corresponding to the specified period of the signal
 
 begin
-    --! Generate clock enable signal. By default, enable signal
-    --! is low and generated pulse is always one clock long.
     p_clk_enable : process (clk) is
     begin
 
-        -- Synchronous proces
-        if (rising_edge(clk)) then
-            -- if high-active reset then
-            if (rst = '1') then
-
-                -- Clear all bits of local counter
-                sig_count <= (others => '0'); 
-                -- Set output `pulse` to low
-                pulse <= '0';
-            -- elsif sig_count is PERIOD-1 then
-            elsif (sig_count = PERIOD-1) then 
-                -- Clear all bits of local counter
-                sig_count <= (others => '0');
-                -- Set output `pulse` to high
-                pulse <= '1';
-            -- else
+        if (rising_edge(clk)) then --condition for rising edge of clk
+            if (rst = '1') then --condition for active rst               
+                sig_count <= (others => '0'); --clear all bits of local counter               
+                pulse <= '0'; -- set output `pulse` to low
+            elsif (sig_count = PERIOD-1) then --condition for value of sig_count value equal to PERIOD-1                
+                sig_count <= (others => '0'); --clear all bits of local counter                
+                pulse <= '1'; --set output `pulse` to high
             else 
-                -- Increment local counter
-                sig_count <= sig_count +1;
-                -- Set output `pulse` to low
-                pulse <= '0';
-            -- Each `if` must end by `end if`
+                
+                sig_count <= sig_count +1; --increment local counter                
+                pulse <= '0'; --set output `pulse` to low
             end if;
         end if;
 
